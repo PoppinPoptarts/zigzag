d6f17a8da636dce0ec62a3303b763522d6adb90b
PoppinPoptarts <77585609+PoppinPoptarts@users.noreply.github.com>
2021-01-23 19:57:48 -0800

    Add files via upload

---
+++ zigzag.py
@@ -0,0 +1,60 @@
+#Made By PoppinPoptarts
+
+#Generate Any amount of ZigZags using this!
+
+
+choice = input("How many times do you want to generate A big wiggle: ")
+variable = """big wiggle
+big wiggle
+ big wiggle
+  big wiggle
+   big wiggle
+     big wiggle
+       big wiggle
+         big wiggle
+            big wiggle
+               big wiggle
+                  big wiggle
+                     big wiggle
+                        big wiggle
+                           big wiggle
+                              big wiggle
+                                 big wiggle
+                                    big wiggle
+                                       big wiggle
+                                         big wiggle
+                                           big wiggle
+                                             big wiggle
+                                              big wiggle
+                                               big wiggle
+                                                big wiggle
+                                                big wiggle
+                                                big wiggle
+                                                big wiggle
+                                               big wiggle
+                                              big wiggle
+                                            big wiggle
+                                         big wiggle
+                                       big wiggle
+                                     big wiggle
+                                   big wiggle
+                                 big wiggle
+                              big wiggle
+                            big wiggle
+                          big wiggle
+                        big wiggle
+                      big wiggle
+                    big wiggle
+                 big wiggle
+               big wiggle
+            big wiggle
+         big wiggle
+      big wiggle
+   big wiggle
+ big wiggle
+big wiggle"""
+
+for i in range(int(choice)):
+    print(variable)
+
+#made using python 3.9.1
